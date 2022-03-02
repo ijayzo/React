6b0e28b2622999e9b02a4b5f48f267bceba01733
qwakumuller <richmondawuahadjei@yahoo.com>
2022-03-02 18:22:04 -0500

    Add Environment Variable for java

--- src/Apis/HotelPartnerApi.js
+++ src/Apis/HotelPartnerApi.js
@@ -1,21 +1,21 @@
 import axios from "axios";
 
 export const createHotelPartner = async(createHotelRequest) => {
-    const response = await axios.post("http://localhost:8080/hotel/new", createHotelRequest)
+    const response = await axios.post(`${process.env.REACT_APP_JAVA_URL}/hotel/new`, createHotelRequest)
     return response.data
 }
 
 export const getAllHotelPartners = async() => {
-    const response = await axios.get("http://localhost:8080/hotel/all")
+    const response = await axios.get(`${process.env.REACT_APP_JAVA_URL}/hotel/all`)
     return response.data
 }
 
 export const getAllHotelPartnersById = async(hotelId) => {
-    const response = await axios.get("http://localhost:8080/hotel/all", {hotelId})
+    const response = await axios.get(`${process.env.REACT_APP_JAVA_URL}/hotel/all`, {hotelId})
     return response.data
 }
 
 export const deleteHotel = async(hotelId) => {
-    const response = await axios.post("http://localhost:8080/hotel/delete", {hotelPartnerId:hotelId})
+    const response = await axios.post(`${process.env.REACT_APP_JAVA_URL}/hotel/delete`, {hotelPartnerId:hotelId})
     return response.data
 }
--- src/Apis/employeeApi.js
+++ src/Apis/employeeApi.js
@@ -1,21 +1,21 @@
 import axios from "axios";
 
 export const employeeLogin = async(loginRequest) => {
-    const response = await axios.post("http://34.111.79.232/auth/login", loginRequest)
+    const response = await axios.post(`${process.env.REACT_APP_JAVA_URL}/auth/login`, loginRequest)
     return response.data
 }
 
 export const createEmployee = async(createEmployeeRequest) => {
-    const response = await axios.post("http://localhost:8080/employee/createEmployee", createEmployeeRequest)
+    const response = await axios.post(`${process.env.REACT_APP_JAVA_URL}/employee/createEmployee`, createEmployeeRequest)
     return response.data
 }
 
 export const getAllEmployee = async() => {
-    const response = await axios.get("http://34.111.79.232/employee/getAllEmployees")
+    const response = await axios.get(`${process.env.REACT_APP_JAVA_URL}/employee/getAllEmployees`)
     return response.data
 }
 
 export const deleteEmployee = async(deleteEmployeeRequest) => {
-    const response = await axios.post("http://localhost:8080/employee/deleteEmployee", {employeeId:deleteEmployeeRequest})
+    const response = await axios.post(`${process.env.REACT_APP_JAVA_URL}/employee/deleteEmployee`, {employeeId:deleteEmployeeRequest})
     return response.data
 }
