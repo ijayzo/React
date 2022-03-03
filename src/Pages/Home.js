643b3f9abace335316d604dfbb0778b019aa3750
qwakumuller <richmondawuahadjei@yahoo.com>
2022-03-02 21:05:02 -0500

    Pass

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,6 +3,7 @@ export default function Home(){
     return( 
         <div>
             <h1>WELCOME TO VACATION</h1>
+            <h2>`${process.env.REACT_APP_JAVA_URL}` </h2>
         </div>
 
     )
