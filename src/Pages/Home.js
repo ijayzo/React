3f8eb39c4e5d95423f9f6f831d3b96836e9b5c2a
qwakumuller <richmondawuahadjei@yahoo.com>
2022-03-03 01:12:32 -0500

    Fix This

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,7 +2,7 @@ export default function Home(){
 
     return( 
         <div>
-            <h1>WELCOME TO VACATION</h1>
+            <h1>WELCOME TO VACATIONs</h1>
             <h2>`${process.env.REACT_APP_JAVA_URL}` </h2>
         </div>
 
