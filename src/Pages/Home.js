83bb3e43fd00f7450cf32ab7dcf28c20dc5454de
qwakumuller <richmondawuahadjei@yahoo.com>
2022-03-03 01:30:23 -0500

    Fix Space

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,7 +2,7 @@ export default function Home(){
 
     return( 
         <div>
-            <h1>WELCOME TO VACATION</h1>
+            <h1>WELCOME TO VACATIONS</h1>
             <h2>`${process.env.REACT_APP_JAVA_URL}` </h2>
         </div>
 
